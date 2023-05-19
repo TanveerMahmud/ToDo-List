d9c6da5e04e840a566288b186a9061b3b8c20af6
TanveerMahmud <117068429+TanveerMahmud@users.noreply.github.com>
2023-05-19 11:03:37 +1000

    minor changes

--- src/components/AddTodo/index.js
+++ src/components/AddTodo/index.js
@@ -1,29 +1,26 @@
 import { Button, TextField } from '@mui/material'
-import { Component } from 'react'
 
-class AddTodo extends Component {
-  render() {
-    return (
-      <>
-        <TextField
-          variant="outlined"
-          label="add todo"
-          size="small"
-          onChange={this.props.handleChange}
-          value={this.props.addInput}
-          sx={{ ml: '20px' }}
-        />
+const AddTodo = ({ handleChange, addInput, handleAdd }) => {
+  return (
+    <>
+      <TextField
+        variant='outlined'
+        label='add todo'
+        size='small'
+        onChange={handleChange}
+        value={addInput}
+        sx={{ ml: '20px' }}
+      />
 
-        <Button
-          variant="contained"
-          onClick={this.props.handleAdd}
-          sx={{ height: '40px', ml: '10px' }}
-        >
-          Add
-        </Button>
-      </>
-    )
-  }
+      <Button
+        variant='contained'
+        onClick={addInput.length > 0 ? handleAdd : ''}
+        sx={{ height: '40px', ml: '10px' }}
+      >
+        Add
+      </Button>
+    </>
+  )
 }
 
 export default AddTodo
